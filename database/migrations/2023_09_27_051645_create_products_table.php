5ff44dfe9b30924d7887e29cd501dd9041df9074
taihoquoc <99245713+taihoquoc@users.noreply.github.com>
2023-09-27 12:23:39 +0700

    update migrate

--- database/migrations/2023_09_27_051645_create_products_table.php
+++ database/migrations/2023_09_27_051645_create_products_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->float('price');
+            $table->integer('brand_id');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
