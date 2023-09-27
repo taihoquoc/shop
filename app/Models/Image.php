10aa315c80b1583f1bda0a1f4c138a2f2efbb7dc
taihoquoc <99245713+taihoquoc@users.noreply.github.com>
2023-09-27 12:44:25 +0700

    process model and relationship

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+
+    protected $table = "images";
+
+    public function product() {
+        return $this->belongsTo('App\Models\Product');
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $table = "products";
+
+    public function user() {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function brand() {
+        return $this->belongsTo('App\Models\Brand');
+    }
+
+    public function images() {
+        return $this->hasMany('App\Models\Image', 'product_id');
+    }
 }
