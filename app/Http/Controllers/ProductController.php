bd3d6d9507ce7fc1e31cb634fa3bb7ae57573729
taihoquoc <99245713+taihoquoc@users.noreply.github.com>
2023-09-30 06:55:50 +0700

    update product create with image

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,107 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $input = $request->all();
+        $user_id = 1;
+        $input['user_id'] = $user_id;
+        $product = Product::create($input);
+        if($request->hasFile('images')) {
+            $images = $request->file('images');
+            foreach($images as $key => $image) {
+                $filename = 'product_'.$product->id.'_'.$key.'.'.$image->extension();
+                $path = $image->storeAs('public/product', $filename);
+                $url = url(Storage::url($path));
+                Image::create([
+                    'product_id' => $product->id,
+                    'filename' => $filename,
+                    'url' => $url
+                ]);
+            }
+        }
+        return response()->json($product);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($product_id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $product_id)
+    {
+        $product = Product::find($product_id);
+        $input = $request->all();
+        $product->fill($input);
+        return response()->json($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
